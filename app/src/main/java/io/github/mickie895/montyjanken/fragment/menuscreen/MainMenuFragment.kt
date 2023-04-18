acf3cc7f7b9bac5419676bd8786005ba1e1fa1e6
mickie895 <mken.mickie@gmail.com>
2023-04-18 10:54:38 +0900

    メイン画面の仮実装

--- app/src/main/java/io/github/mickie895/montyjanken/fragment/menuscreen/MainMenuFragment.kt
+++ app/src/main/java/io/github/mickie895/montyjanken/fragment/menuscreen/MainMenuFragment.kt
@@ -2,11 +2,16 @@ package io.github.mickie895.montyjanken.fragment.menuscreen
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.InflateException
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.navigation.fragment.findNavController
 import io.github.mickie895.montyjanken.R
+import io.github.mickie895.montyjanken.databinding.FragmentMainMenuBinding
 
 class MainMenuFragment : Fragment() {
 
@@ -14,12 +19,14 @@ class MainMenuFragment : Fragment() {
         fun newInstance() = MainMenuFragment()
     }
 
-    private lateinit var viewModel: MainMenuViewModel
+    private val viewModel: MainMenuViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainMenuViewModel::class.java)
-        // TODO: Use the ViewModel
+        val binding = FragmentMainMenuBinding.inflate(layoutInflater)
+        binding.startMainButton.setOnClickListener{
+            findNavController().navigate(R.id.action_mainMenuFragment_to_firstHandSelectFragment)
+        }
     }
 
     override fun onCreateView(
